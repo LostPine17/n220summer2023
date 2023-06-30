9098ff6399137c5a696de38a987cad2b4370106e
LostPine17 <shvidston@yahoo.com>
2023-06-30 17:36:36 -0400

    Lab 1 Complete

---
+++ labs/lab1/app.js
@@ -0,0 +1,30 @@
+function setup() {
+    createCanvas(800, 600);
+
+    stroke(200, 200, 200)
+    strokeWeight(1)
+    rect(65, 55, 510,510);
+    fill("black")
+    circle(320, 310, 250);
+    fill("red");
+    triangle (66, 400, 320, 56, 574, 400)
+    fill("orange")
+    triangle (76, 395, 320, 66, 564, 395)
+    fill("yellow")
+    triangle (86, 390, 320, 76, 554, 390)
+    fill("green")
+    triangle (96, 385, 320, 86, 544, 385)
+    fill("blue")
+    triangle (106, 380, 320, 96, 534, 380)
+    fill("purple")
+    triangle (116, 375, 320, 106, 524, 375)
+    fill("white")
+    triangle (126, 370, 320, 116, 514, 370)
+
+    let myName = "S. Hvidston";
+    console.log(myName);
+
+    let date = "June 30th 2023";
+    console.log(date);
+
+}
