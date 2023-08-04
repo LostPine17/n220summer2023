12f25ff9b860a38eb4ec95d26303d0efc01e3af1
LostPine17 <shvidston@yahoo.com>
2023-08-04 14:18:23 -0400

    Lab 10.2 extra comments

--- labs/lab10.2/app.js
+++ labs/lab10.2/app.js
@@ -1,20 +1,26 @@
-let r, g, b;
+let r, g, b; // Declare variables for red, green, and blue color components
+
+// Declare variables to store references to DOM elements
 let colorDiv, currentColorDiv;
 let buttons;
 
 function setup() {
   noCanvas();
 
-  r = g = b = 0;
+  r = g = b = 0; // Initialize red, green, and blue color components to 0
+
+  // Select the colorDiv and currentColorDiv elements from the DOM using their IDs
   colorDiv = select("#colorDiv");
   currentColorDiv = select("#currentColorDiv");
-  buttons = selectAll("button");
+  buttons = selectAll("button"); // Select all the buttons in the DOM and store them in an array
 
+  // Attach a 'mouseClicked' event listener to each button in the array
   for (let i = 0; i < buttons.length; i++) {
     buttons[i].mouseClicked(changeColor);
   }
 }
 
+// Function to handle color change when a button is clicked
 function changeColor() {
   // Get the color and value from the button's data attributes
   let color = this.elt.getAttribute("data-color");
